9f8cccd634a7a45d72f50ef2affe0b2a8904d50c
tony atoms <tony@atoms.net>
2014-01-06 17:46:09 -0600

    just an end of day slightly beyoond Hello World commit

--- mobile/Titanium/glissando/Resources/app.js
+++ mobile/Titanium/glissando/Resources/app.js
@@ -9,18 +9,18 @@ var tabGroup = Titanium.UI.createTabGroup();
 // create base UI tab and root window
 //
 var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
+    title:'Glissando Input Panel',
     backgroundColor:'#fff'
 });
 var tab1 = Titanium.UI.createTab({  
     icon:'KS_nav_views.png',
-    title:'Tab 1',
+    title:'Input Panel',
     window:win1
 });
 
 var label1 = Titanium.UI.createLabel({
 	color:'#999',
-	text:'This Be Window 1',
+	text:'This is your soon to come INPUT PANEL!',
 	font:{fontSize:20,fontFamily:'Helvetica Neue'},
 	textAlign:'center',
 	width:'auto'
@@ -32,18 +32,18 @@ win1.add(label1);
 // create controls tab and root window
 //
 var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
+    title:'Glissando Output Panel',
     backgroundColor:'#fff'
 });
 var tab2 = Titanium.UI.createTab({  
     icon:'KS_nav_ui.png',
-    title:'Tab 2',
+    title:'Outputty',
     window:win2
 });
 
 var label2 = Titanium.UI.createLabel({
 	color:'#999',
-	text:'Behold the mighty Window 2',
+	text:'Just pay for the record already... jeez',
 	font:{fontSize:20,fontFamily:'Helvetica Neue'},
 	textAlign:'center',
 	width:'auto'
