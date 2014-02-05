787f977d0b52ff902c1bcbb22cd656e6c1ad66a7
tony atoms <tony@atoms.net>
2014-02-05 16:22:36 -0600

    another test  of Xcode local commits - make nag bar visible, add back button, tweak results table display fonts

--- iOS/glissando/glissando/ResultsTableViewController.m
+++ iOS/glissando/glissando/ResultsTableViewController.m
@@ -5,6 +5,9 @@
 //  Created by Tony Adams on 1/22/14.
 //  Copyright (c) 2014 atoms.net. All rights reserved.
 //
+/*
+ ArtistCarianiArtist InfoItalian, 1485/1490 - 1547 or afterTitleA ConcertDatedc. 1518-1520Mediumoil on canvasClassificationPaintingDimensionsoverall: 92 x 130 cm (36 1/4 x 51 3/16 in.) framed: 128.8 x 168.1 x 6.4 cm (50 11/16 x 66 3/16 x 2 1/2 in.)CreditBequest of Lore Heinemann in memory of her husband, Dr. Rudolf J. HeinemannAccession No.1997.57.2DigitizationDirect Digital CaptureImage UseOpen Access
+*/
 
 #import "ResultsTableViewController.h"
 #import "iTunesItem.h"
@@ -20,10 +23,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+   
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Cell"];
     
-    // 
     JSONLoader *jsonLoader = [[JSONLoader alloc] init];
     
     NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"https://itunes.apple.com/search?term=%@", self.searchString]];
@@ -31,6 +33,7 @@
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         
         _products = [jsonLoader itemsFromJSONFetch:url withKey:@"results"];
+        
         // Now that we have the data, reload the table data on the main UI thread
         [self.tableView performSelectorOnMainThread:@selector(reloadData) withObject:nil waitUntilDone:YES];
     });
@@ -55,22 +58,46 @@
     //    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     
     //experimental
-    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
-
+   UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+   
+ // no joy
+ //   UIImageView *productImageView = (UIImageView *)[cell viewWithTag:100];
+ //   productImageView.image = [UIImage imageNamed:@"noArtAvailable.jpg"];
+    
+ // works but underlays the text (if going this route everything will have to be customized?)
+  //  UIImageView * imageView = [[UIImageView alloc] initWithFrame:CGRectMake(5, 40, 64, 64)];
+ //   imageView.image = [UIImage imageNamed:@"noArtAvailable.jpg"];
+ //   [cell.contentView addSubview:imageView];
+    
+    
+    cell.imageView.image = [UIImage imageNamed:@"noArtAvailable.jpg"];
     
     iTunesItem *itunesItem = [_products objectAtIndex:indexPath.row];
-
     
    // NSMutableAttributedString *str = [[NSMutableAttributedString alloc] initWithString:@"Using NSAttributed String"];
     
-    cell.textLabel.text = [NSString stringWithFormat:@"artist:%@",itunesItem.artistName ];
-    cell.detailTextLabel.text = [NSString stringWithFormat:@"%@ price: $%@ adjustedPrice: %@", itunesItem.trackName, itunesItem.trackPrice, [self adjustedPriceFromBasePrice:itunesItem.trackPrice]                    ];
     
+    NSString *trackName;
     
+    if (itunesItem.trackName) {
+        trackName = itunesItem.trackName;
+    } else {
     
-    cell.textLabel.font = [UIFont boldSystemFontOfSize: 14.0];
+        trackName = @"[untitled]";
+    }
+    cell.textLabel.font = [UIFont boldSystemFontOfSize: 12.0];
     
-    cell.detailTextLabel.font = [UIFont boldSystemFontOfSize: 12.0];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@",itunesItem.artistName ];
+    
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%@ price: $%@ adjustedPrice: %@", trackName,
+                                 [NSString stringWithFormat:@"%.2f", [itunesItem.trackPrice floatValue] ],
+                                 [self adjustedPriceFromBasePrice:itunesItem.trackPrice]
+                                 ];
+    
+    UILabel *artistNameLabel = (UILabel *)[cell viewWithTag:101];
+    artistNameLabel.text = itunesItem.artistName;
+    
+    cell.detailTextLabel.font = [UIFont systemFontOfSize: 10.0];
     
     //TODO: add lazy loading of artworkUrl30 via https://github.com/rs/SDWebImage or something like that
     //cell.imageView.image = [UIImage imageNamed:@"genericProduct.png"]; //artworkUrl30
@@ -80,9 +107,18 @@
 
 // A fake reduction for working on the table output
 -(NSString*)adjustedPriceFromBasePrice:(NSNumber *)basePrice {
-
-    NSNumber *result = [ NSNumber numberWithFloat: [basePrice floatValue]  * 0.8 ];
-
+ 
+    NSNumber *result;
+    
+    if (basePrice > 0) {
+    
+        result = [ NSNumber numberWithFloat: [basePrice floatValue]  * 0.8 ];
+    
+    } else {
+    
+        result = [ NSNumber numberWithFloat: [basePrice floatValue]];
+    }
+    
     return [NSString stringWithFormat:@"$%.2f", [result floatValue] ];
 }
 
